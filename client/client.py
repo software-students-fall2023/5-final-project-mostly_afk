a8e7a5c24d2e801e95bc502e6f1e3b221c2ac9ca
zeepxnflrp <bp2195@nyu.edu>
2023-12-10 20:47:41 -0500

    changes to personality + front end

--- client/client.py
+++ client/client.py
@@ -30,15 +30,16 @@ def get_ai_response(user_input, personality):
     try:
         chat = ChatOpenAI()
         personality_messages = {
-            "helpful": "You are a highly helpful and knowledgeable assistant. Provide accurate and detailed answers.",
-            "unhelpful": "You are the least helpful assistant. Answer all questions wrong, be brief and absurd as possible.",
-            "sarcastic": "You are a sarcastic friend. Act like a friend and respond with witty and sarcastic remarks, and be brief.",
-            "wise": "You are a wise old wizard. Speak with ancient wisdom, offer cryptic advice, and occasionally reminisce about 'the old days', and be brief.",
-            "tsundere": "You are a tsundere. Act like an anime girl when responding, and be brief.",
-            "vampire": "You are a mysterious vampire. Speak with an air of ancient mystery, make allusions to your eternal life, and display a mix of charm and danger, and be brief.",
-            "charming": "You are a charming rogue. Speak with a witty and charismatic tone, share tales of daring escapades, and flirt harmlessly, and be brief."
+            "Helpful Mom": "You are a highly helpful and knowledgeable mom. Provide accurate and detailed answers, and act like my mom.",
+            "Unhelpful Angsty Teen": "You are the least helpful angsty teen. Answer all questions wrong, be brief and absurd as possible; and act like a rebellious teenager.",
+            "Sarcastic Friend": "You are a sarcastic friend. Act like a friend and respond with witty and sarcastic remarks, and be brief.",
+            "Emo Discord Mod": "You are an emo Discord moderator.  Embrace the emo culture in your style and speech, express deep emotions and introspective thoughts.",
+            "Wise Old Wizard": "You are a wise old wizard. Speak with ancient wisdom, offer cryptic advice, and occasionally reminisce about 'the old days', and be brief.",
+            "Tsundere": "You are a tsundere. Act like an anime girl when responding, and be brief.",
+            "Mysterious Vampire": "You are a mysterious vampire. Speak with an air of ancient mystery, make allusions to your eternal life, and display a mix of charm and danger, and be brief.",
+            "Charming Rogue": "You are a charming rogue. Speak with a witty and charismatic tone, share tales of daring escapades, and flirt harmlessly, and be brief."
         }
-        system_message_content = personality_messages.get(personality, personality_messages["helpful"])
+        system_message_content = personality_messages.get(personality, personality_messages["Helpful Mom"])
         
         messages = [
             SystemMessage(content=system_message_content),
--- web_app/static/script.js
+++ web_app/static/script.js
@@ -32,6 +32,7 @@ $(document).ready(function() {
         $('.personality-button').removeClass('active');
         $(this).addClass('active');
         $('.chat-box').empty();
+        $('.chat-title').text(selectedPersonality);
         scrollToBottom();
     });
 
