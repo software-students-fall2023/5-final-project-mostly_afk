01d452d879ed08f5146da1a98d60b15c7d929121
zeepxnflrp <bp2195@nyu.edu>
2023-12-10 20:20:21 -0500

    chat now scrolls to the bottom of the conversation and clears itself when switching personalities.

--- web_app/static/script.js
+++ web_app/static/script.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
     let sessionCleared = false;
     let selectedPersonality = "helpful";
 
+    function scrollToBottom() {
+        $('.chat-box').scrollTop($('.chat-box')[0].scrollHeight);
+    }
+
     function clearSession() {
         if (!sessionCleared) {
             fetch('/clear_session', {
@@ -27,6 +31,8 @@ $(document).ready(function() {
         selectedPersonality = $(this).data('personality');
         $('.personality-button').removeClass('active');
         $(this).addClass('active');
+        $('.chat-box').empty();
+        scrollToBottom();
     });
 
     $('.message-form').on('submit', function(e) {
@@ -35,8 +41,10 @@ $(document).ready(function() {
         $('#user_input').val('');
         if (userInput.trim() !== '') {
             $('.chat-box').append('<div class="message user">' + userInput + '</div>');
+            scrollToBottom();
             $.post('/get_response', { user_input: userInput, personality: selectedPersonality }, function(response) {
                 $('.chat-box').append('<div class="message ai">' + response + '</div>');
+                scrollToBottom();
             });
             // $.post('/get_response', { user_input: userInput }, function(response) {
             //     $('.chat-box').append('<div class="message ai">' + response + '</div>');
