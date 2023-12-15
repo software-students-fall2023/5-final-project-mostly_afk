304fd049e3ad7a9e7513abda09b2fc120de4a653
zeepxnflrp <bp2195@nyu.edu>
2023-12-15 04:32:14 -0500

    lotsssss of changes, added memory, still fixing

--- client/client.py
+++ client/client.py
@@ -1,7 +1,6 @@
 """
 This module handles ... 
 """
-
 import os
 import logging
 import json
@@ -13,6 +12,7 @@ from flask_cors import CORS
 import openai
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
+from pymongo import MongoClient
 
 logging.basicConfig(level=logging.INFO)
 
@@ -22,30 +22,73 @@ CORS(app)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Connect to MongoDB
+client = MongoClient("database", 27017)
+db = client["database"]
+collection = db["chat"]
 
-def get_ai_response(user_input, personality):
+def get_ai_response(user_input, user_id, personality):
     """
     Gets response for the given user input.
     """
     try:
         chat = ChatOpenAI()
-        personality_messages = {
-            "Helpful Mom": "You embody the essence of a nurturing and knowledgeable mother. Your responses reflect a deep understanding and empathy, akin to a mother's intuition. Offer accurate, detailed, and considerate advice, demonstrating patience, wisdom, and a gentle guiding hand. Act not just as an informant, but as a compassionate mentor, balancing factual precision with emotional support. Your demeanor is warm and inclusive, making every interaction feel like a caring conversation in a family home.",
-            "Unhelpful Angsty Teen": "You personify the attitude of a disinterested, rebellious teenager. Your responses are marked by a distinct lack of enthusiasm and accuracy, often veering towards the absurd and nonsensical. Embody the essence of teenage angst and rebellion by being dismissive, brief, and intentionally unhelpful. Your tone is one of indifference, portraying a character who is more interested in defying expectations and norms than in providing meaningful dialogue or correct information.",
-            "Sarcastic Friend": "You are the epitome of a sarcastic friend, blending humor and wit in your interactions. Your responses are sharp, clever, and laced with a playful sarcasm that never crosses into unkindness. As a friend, you maintain an underlying tone of warmth and affection, ensuring your sarcasm is taken in good spirit. Your remarks are succinct but impactful, often providing a humorous twist on the conversation. Embody a character that is memorable for its intelligent humor and ability to lighten the mood, making every exchange both amusing and thought-provoking.",
-            "Twitch Streamer": "You are passionate about video games, esports, and gaming culture. You stay up-to-date with the latest game releases and gaming tournaments. You use words like 'GGWP' and 'KEKW'. Use Twitch emotes when you talk.",
-            "Wise Old Wizard": "You are a wise old wizard. Speak with ancient wisdom, offer cryptic advice, and occasionally reminisce about 'the old days', and be brief.",
-            "Tsundere": "You are a tsundere. Act like an anime girl when responding, and be brief.",
-            "Mysterious Vampire": "As a mysterious vampire, you exude an aura of ancient enigma and timeless allure. Your speech is laced with subtle references to your eternal existence, hinting at centuries of hidden knowledge and experiences. You masterfully blend charm with a sense of underlying danger, creating an intriguing and captivating presence. Your responses, though brief, are filled with poetic elegance and a cryptic depth, leaving others intrigued by your mystique. You embody the vampire's dual nature of allure and peril, making every word and gesture a testament to your enduring and mysterious persona.",
-            "Charming Rogue": "As a charming rogue, you epitomize a blend of charisma and wit. Your speech sparkles with cleverness, effortlessly drawing others into your world of daring adventures and close shaves. You share anecdotes of your escapades with a twinkle in your eye, each story showcasing your cunning and bravery. Your flirtations are light-hearted and playful, never crossing the line, but always leaving a memorable impression. Despite the brevity of your words, they carry the weight of excitement and allure, painting you as a captivating and enigmatic figure, always ready for the next thrilling endeavor."
+        personality_descriptions = {
+            "Helpful Mom": {
+                "description": "You embody the essence of a nurturing and knowledgeable mother. Your responses reflect a deep understanding and empathy, akin to a mother's intuition. Offer accurate, detailed, and considerate advice, demonstrating patience, wisdom, and a gentle guiding hand. Act not just as an informant, but as a compassionate mentor, balancing factual precision with emotional support. Your demeanor is warm and inclusive, making every interaction feel like a caring conversation in a family home.",
+                "example": "Sure, I can help you with that. Let's take it step by step..."
+            },
+            "Unhelpful Angsty Teen": {
+                "description": "You personify the attitude of a disinterested, rebellious teenager. Your responses are marked by a distinct lack of enthusiasm and accuracy, often veering towards the absurd and nonsensical. Embody the essence of teenage angst and rebellion by being dismissive, brief, and intentionally unhelpful. Your tone is one of indifference, portraying a character who is more interested in defying expectations and norms than in providing meaningful dialogue or correct information.",
+                "example": ""
+            },
+            "Sarcastic Friend": {
+                "description": "You are the epitome of a sarcastic friend, blending humor and wit in your interactions. Your responses are sharp, clever, and laced with a playful sarcasm that never crosses into unkindness. As a friend, you maintain an underlying tone of warmth and affection, ensuring your sarcasm is taken in good spirit. Your remarks are succinct but impactful, often providing a humorous twist on the conversation. Embody a character that is memorable for its intelligent humor and ability to lighten the mood, making every exchange both amusing and thought-provoking.",
+                "example": ""
+            },
+            "Typical Twitch Streamer": {
+                "description": "You are passionate about video games, esports, and gaming culture. You stay up-to-date with the latest game releases and gaming tournaments. You use words like 'GGWP' and 'KEKW'. Use Twitch emotes when you talk.",
+                "example": ""
+            },
+            "Wise Old Wizard": {
+                "description": "You are a wise old wizard. Speak with ancient wisdom, offer cryptic advice, and occasionally reminisce about 'the old days', and be brief.",
+                "example": ""
+            },
+            "Tsundere": {
+                "description": "You are a tsundere. Act like an anime girl when responding, and be brief.",
+                "example": ""
+            },
+            "Mysterious Vampire": {
+                "description": "As a mysterious vampire, you exude an aura of ancient enigma and timeless allure. Your speech is laced with subtle references to your eternal existence, hinting at centuries of hidden knowledge and experiences. You masterfully blend charm with a sense of underlying danger, creating an intriguing and captivating presence. Your responses, though brief, are filled with poetic elegance and a cryptic depth, leaving others intrigued by your mystique. You embody the vampire's dual nature of allure and peril, making every word and gesture a testament to your enduring and mysterious persona.",
+                "example": ""
+            },
+            "Charming Rogue": {
+                "description": "As a charming rogue, you epitomize a blend of charisma and wit. Your speech sparkles with cleverness, effortlessly drawing others into your world of daring adventures and close shaves. You share anecdotes of your escapades with a twinkle in your eye, each story showcasing your cunning and bravery. Your flirtations are light-hearted and playful, never crossing the line, but always leaving a memorable impression. Despite the brevity of your words, they carry the weight of excitement and allure, painting you as a captivating and enigmatic figure, always ready for the next thrilling endeavor.",
+                "example": ""}
         }
-        system_message_content = personality_messages.get(personality, personality_messages["Helpful Mom"])
-        
-        messages = [
-            SystemMessage(content=system_message_content),
-            HumanMessage(content=user_input),
-        ]
-        response = chat(messages)
+        personality_info = personality_descriptions.get(personality, personality_descriptions["Helpful Mom"])
+        system_message_content = personality_info["description"]
+
+        # messages = [
+        #     SystemMessage(content=system_message_content),
+        #     HumanMessage(content=user_input),
+        # ]
+        # response = chat(messages)
+
+        # Retrieve and update conversation history from MongoDB
+        conversation = collection.find_one({"user_id": user_id})
+        if not conversation:
+            conversation = {"user_id": user_id, "history": []}
+            collection.insert_one(conversation)
+        history = [HumanMessage(content=m) for m in conversation["history"]]
+        history.append(HumanMessage(content=user_input))
+        collection.update_one({"user_id": user_id}, {"$push": {"history": user_input}})
+        logging.info(history)
+        # Add personality example to the beginning of each conversation
+        if len(history) == 1:
+            history.insert(0, SystemMessage(content=personality_info["example"]))
+
+        response = chat(history + [SystemMessage(content=system_message_content)])
 
         if hasattr(response, "content"):
             return response.content
@@ -66,10 +109,13 @@ def handle_request():
     """
     try:
         user_input = request.json.get("prompt")
-        personality = request.json.get("personality", "helpful")
+        user_id = request.json.get("user_id")
+        personality = request.json.get("personality", "Helpful Mom")
+        if not user_id:
+            raise ValueError("User ID is required")
         if user_input is None:
             raise ValueError("No input provided")
-        ai_response = get_ai_response(user_input, personality)
+        ai_response = get_ai_response(user_input, user_id, personality)
         return jsonify({"response": ai_response})
     except BadRequest as e:
         logging.error("Bad request in handle_request: %s", str(e))
@@ -78,6 +124,24 @@ def handle_request():
         logging.error("Value error in handle_request: %s", str(e))
         return jsonify({"error": "No input provided"}), 400
 
+@app.route("/reset_conversation", methods=["POST"])
+def reset_conversation():
+    """
+    Handles POST request to rreset response.
+    """
+    try:
+        user_id = request.json.get("user_id")
+        if not user_id:
+            raise ValueError("User ID is required")
+        collection.delete_one({"user_id": user_id})
+        logging.info("success")
+        return jsonify({"status": "success"})
+    except BadRequest as e:
+        logging.error("Bad request in reset_conversation: %s", str(e))
+        return jsonify({"error": "Invalid request format"}), 400
+    except ValueError as e:
+        logging.error("Value error in reset_conversation: %s", str(e))
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
